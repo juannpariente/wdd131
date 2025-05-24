81d06e51ee5e3f79802e2e24032b1edd1eb025ba
juannpariente <jpariente000@gmail.com>
2025-05-24 08:45:16 -0300

    Create course home page

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const year = document.querySelector("#currentyear");
+
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+const lastModified = document.querySelector("#lastmodified");
+lastModified.innerHTML = document.lastModified;
+
